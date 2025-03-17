353c7f9ec5c7e78d73f6ce1bdc8b83be623eb706
82106 <dudcjsv1746@naver.com>
2025-03-18 01:58:03 +0900

    리액트 카드리스트 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Image } from 'react-bootstrap'; // 꼭 import를 해와야한다
+import { Card, Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <div class="row row-cols-1 row-cols-md-3 g-3">
+      <div class="col">
+       <div class="card h-100">
+            <Card>
+              <Card.Img variant="top" src='' />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>Card text</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+    
+       </div>
+      </div>
+      <div class="col">
+       <div class="card h-100">
+            <Card>
+              <Card.Img variant="top" src='' />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>Card text</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+    
+       </div>
+      </div>
+      <div class="col">
+       <div class="card h-100">
+            <Card>
+              <Card.Img variant="top" src='' />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>Card text</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+    
+       </div>
+      </div>
+      <div class="col">
+       <div class="card h-100">
+            <Card>
+              <Card.Img variant="top" src='' />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>Card text</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+    
+       </div>
+      </div>
     </div>
   );
 }
