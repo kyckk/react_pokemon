c884c1f5c93522ac12bb290e6228db4fb8ca6047
82106 <dudcjsv1746@naver.com>
2025-03-19 02:56:05 +0900

    이미지파일 올리기

--- src/component/PokeCard.jsx
+++ src/component/PokeCard.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Image } from 'react-bootstrap'; // 꼭 import를 해와야한다
 import { Card, Button } from "react-bootstrap";
+import abc from "../images/abc.png"
 const PokeCard = ({ header, data = [], footer }) => {
   console.log(data);
   return (
@@ -8,12 +9,12 @@ const PokeCard = ({ header, data = [], footer }) => {
       {header && <header>{header}</header>}
       <div class="row row-cols-1 row-cols-md-3 g-3">
       {
-        data.map(({x,index})=> (
-          
+        data.map((item)=> (
+            
             <div class="col">
-            <div class="card h-100" key={index}>
+            <div  key={item.id} class="card h-100">
               <Card>
-                <Card.Img variant="top" src="" />
+                <Card.Img  variant="top" src={abc} / >
                 <Card.Body>
                   <Card.Title>Card title</Card.Title>
                   <Card.Text>Card text</Card.Text>
--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -2,11 +2,13 @@ import Card from "../../component/PokeCard";
 import Page from "../../component/Page";
 function List() {
   return (
+    
     <Page>
+      
       <Card
         data={[
-          { term: "주문일시", description: "하이이" },
-          { term: "주문번호", description: "하ㅏ이" },
+          {id:"1" ,term: "주문일시", description: "하이이" },
+          { id:"2", term: "주문번호", description: "하ㅏ이" },
         ]}
       ></Card>
     </Page>
