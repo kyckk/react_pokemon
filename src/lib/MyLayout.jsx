919fea19df041cb3a51a9614dc6c0ce34aa9831c
youngcheon <dudcjsv1746@naver.com>
2025-04-08 16:50:48 +0900

    다이얼로 클릭비동기화아직못함함

--- src/lib/MyLayout.jsx
+++ src/lib/MyLayout.jsx
@@ -1,13 +1,19 @@
-import React, { useState, createContext, useContext } from "react";
+import React, { createContext, useState, useContext } from "react";
 import BackDrop from "../component/BackDrop";
 
+// 컨텍스트 생성
+export const LayoutContext = createContext({});
+LayoutContext.displayName = "LayoutContext";
 
-export const LayoutContext = createContext();
-
+// Layout 컴포넌트: 상태 관리 및 Context Provider 제공
 export const Layout = ({ children }) => {
-  const [dialog, setDialog] = useState(null);
-  const value = { dialog, setDialog };
-
+  const [dialog, setDialog] = useState("");
+  console.log(children)
+  const value = {
+    dialog,
+    setDialog, // 필요 시 하위에서 dialog 업데이트 가능
+  };
+ 
   return (
     <LayoutContext.Provider value={value}>
       {children}
@@ -20,5 +26,6 @@ export const DialogContainer = () => {
 
   return dialog ? <BackDrop>{dialog}</BackDrop> : null;
 };
+
 
 
